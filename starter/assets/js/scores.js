b7713bfef3b83b70d47c7f21a2196b2770c8ff77
salala1005 <jin.mijeong@gmail.com>
2023-12-06 09:24:35 +0000

    render score added

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,7 +1,24 @@
 var highScores = document.querySelector("#highscores")
-
+var savedScores = JSON.parse(localStorage.getItem("scoreTest"))|| []
 
 function renderScores() {
-  highScores.textContent = localStorage.getItem(initialsInput)
-  highScores.textContent = localStorage.getItem(sec)
+  // highScores.textContent = "Initial :  " 
+  // highScores.textContent += JSON.parse(localStorage.getItem("initials")) 
+  // highScores.textContent += " ,  Your score:  " 
+  // highScores.textContent += JSON.parse(localStorage.getItem("scores"))
+for (let index = 0; index < savedScores.length; index++) {
+  console.log(savedScores[index])
+  highScores.innerHTML += "Initials : " + savedScores[index].initials + ". Your score: " + savedScores[index].scores + " <br>"
 }
+
+}
+renderScores();
+
+
+// localStorage.setItem("scoreTest",JSON.stringify([{
+//   initials:"mnb",
+//   scores:"66"
+// }]))
+// var testScores = JSON.parse(localStorage.getItem("scoreTest"))
+// testScores = [...testScores,{initials:"jmj", scores:"12"}]
+// localStorage.setItem("scoreTest",JSON.stringify(testScores))
